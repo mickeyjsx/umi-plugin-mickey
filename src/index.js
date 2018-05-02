d80d53aef557c3dba6efd8098a181355421cd80e
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-05-02 17:08:29 +0800

    specify entry file with options

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-export default function (api) {
+import path from 'path'
+
+export default function (api, options = {}) {
   const { RENDER } = api.placeholder
+  const { entry = './src/index.jsx' } = options
+  const filepath = path.resolve(process.cwd(), entry)
 
   api.register('modifyEntryFile', ({ memo }) => memo.replace(
     RENDER,
     `
-    const start = require('../../start').default;
+    const start = require('${filepath}').default;
     start(require('./router').default)
     `,
   ))

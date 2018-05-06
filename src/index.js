68a4d25a04c735d57582ade35676a4aa31c092f2
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-05-07 01:00:52 +0800

    disable filehash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default function (api, options = {}) {
+  const {
+    entry = './src/index.jsx',
+    name = 'main',
+    enableFileHash = false,
+  } = options
+
   const { RENDER } = api.placeholder
-  const { entry = './src/index.jsx' } = options
   const filepath = path.resolve(process.cwd(), entry)
 
   api.register('modifyEntryFile', ({ memo }) => memo.replace(
@@ -12,4 +18,29 @@ export default function (api, options = {}) {
     start(require('./router').default)
     `,
   ))
+
+  if (!enableFileHash || name) {
+    api.register('modifyWebpackConfig', ({ memo }) => {
+      if (name) {
+        memo.entry = { [name]: memo.entry.umi }
+      }
+
+      if (!enableFileHash) {
+        memo.output.filename = '[name].js'
+        memo.output.chunkFilename = '[name].async.js'
+        memo.plugins = memo.plugins.map((plugin) => {
+          if (plugin instanceof ExtractTextPlugin) {
+            return new ExtractTextPlugin({
+              filename: '[name].css',
+              allChunks: true,
+            })
+          }
+
+          return plugin
+        })
+      }
+
+      return memo
+    })
+  }
 }

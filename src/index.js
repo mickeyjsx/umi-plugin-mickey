f1d428917326c55d8c10cd0669296f60b96ba32f
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-10-11 17:46:49 +0800

    v2.0.0

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ export default function (api, options = {}) {
     entry = './src/index.jsx',
   } = options
 
-  const { RENDER } = api.placeholder
   const filepath = path.resolve(process.cwd(), entry)
 
-  api.register('modifyEntryFile', ({ memo }) => memo.replace(
-    RENDER,
-    `
-    const start = require('${filepath}').default;
-    start(require('./router').default)
-    `,
-  ))
+  api.addEntryCodeAhead(`
+  const start = require('${filepath}').default;
+  start(require('./router').default)
+  `)
 }
